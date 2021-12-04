af4cfb662975224f43be7752a033556c0d84fc35
Erick Namukolo <48956709+ericknamukolo@users.noreply.github.com>
2021-12-04 13:12:35 +0200

    added new skill icon

--- lib/providers/skills.dart
+++ lib/providers/skills.dart
@@ -6,7 +6,7 @@ class Skills with ChangeNotifier {
     Skill(imgPath: 'assets/html5.png', skillName: 'HTML'),
     Skill(imgPath: 'assets/css3.png', skillName: 'CSS'),
     Skill(imgPath: 'assets/javascript.png', skillName: 'JS'),
-    Skill(imgPath: 'assets/react.png', skillName: 'React Js'),
+    Skill(imgPath: 'assets/git.png', skillName: 'Git'),
     Skill(imgPath: 'assets/adobe-xd.png', skillName: 'Abobe XD'),
     Skill(imgPath: 'assets/flutter.png', skillName: 'Flutter'),
     Skill(imgPath: 'assets/dart.png', skillName: 'Dart'),
