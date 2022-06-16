2f0ce9f3911df4c25573ee56861efc28391f59ea
Erick Namukolo <erickmndev@gmail.com>
2022-06-16 10:39:56 +0200

    reduced the icon sizes

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -4,30 +4,30 @@ import 'package:portfolio/constants/colors.dart';
 const kTextStyleWhite = TextStyle(
   color: Colors.white,
   fontWeight: FontWeight.bold,
-  fontSize: 16,
+  fontSize: 14,
 );
 const kNormalTextStyleWhite = TextStyle(
   color: Colors.white,
-  fontSize: 16,
+  fontSize: 14,
 );
 
 const kMiniTitleTextStyleWhite = TextStyle(
   color: Colors.white,
-  fontSize: 18,
+  fontSize: 16,
 );
 
 const kMiniTitleTextStylePink = TextStyle(
   color: kprimaryColor,
-  fontSize: 18,
+  fontSize: 16,
   fontWeight: FontWeight.bold,
 );
 
 const kNormalTextStyleGrey = TextStyle(
   color: Color(0xffB3A595),
-  fontSize: 16,
+  fontSize: 14,
 );
 
 const kTitleTextStyle = TextStyle(
   color: Colors.white,
-  fontSize: 50,
+  fontSize: 48,
 );
