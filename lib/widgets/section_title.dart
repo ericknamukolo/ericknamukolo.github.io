736f6b763c941d0fa133be8f47cb133176854627
Erick Namukolo <erickmndev@gmail.com>
2022-09-25 14:16:53 +0200

    made the desktop about section fully responsive

---
+++ lib/widgets/section_title.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../constants/colors.dart';
+import '../constants/constants.dart';
+
+class SectionTitle extends StatelessWidget {
+  final String title;
+  const SectionTitle({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 50.0),
+      child: Column(
+        children: [
+          Text(
+            title,
+            style: kMiniTitleTextStyleWhite,
+          ),
+          SizedBox(height: 5.0),
+          Container(
+            color: kprimaryColor,
+            height: 4,
+            width: 80,
+          ),
+        ],
+      ),
+    );
+  }
+}
