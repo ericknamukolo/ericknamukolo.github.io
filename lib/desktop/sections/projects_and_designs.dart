0aa9eea1fb6ee8dc903c11ebe744f6229fcb6b82
Erick Namukolo <erickmndev@gmail.com>
2023-09-01 20:51:32 +0200

    did the same for desktop view

--- lib/desktop/sections/projects_and_designs.dart
+++ lib/desktop/sections/projects_and_designs.dart
@@ -5,14 +5,26 @@ import 'package:portfolio/desktop/widgets/project_card.dart';
 import 'package:portfolio/providers/projects.dart';
 import 'package:provider/provider.dart';
 
+import '../../mobile/widgets/tab_btn.dart';
+import '../../models/project.dart';
+import '../../models/tab_btn.dart';
 import '../../widgets/custom_loading_widget.dart';
 import '../../widgets/section_title.dart';
 import '../projects_desktop_screen.dart';
 import '../widgets/mordern_button.dart';
 
-class ProjectsAndDesigns extends StatelessWidget {
+class ProjectsAndDesigns extends StatefulWidget {
   const ProjectsAndDesigns({Key? key}) : super(key: key);
 
+  @override
+  State<ProjectsAndDesigns> createState() => _ProjectsAndDesignsState();
+}
+
+class _ProjectsAndDesignsState extends State<ProjectsAndDesigns> {
+  List<TabButton> tabs = [
+    TabButton(title: 'Personal Projects', icon: Icons.folder, isSelected: true),
+    TabButton(title: 'Work/Client Projects', icon: Icons.laptop_mac_rounded),
+  ];
   @override
   Widget build(BuildContext context) {
     double _screenWidth = MediaQuery.of(context).size.width;
@@ -26,16 +38,54 @@ class ProjectsAndDesigns extends StatelessWidget {
       child: Column(
         children: [
           SectionTitle(title: 'Projects'),
+          Container(
+            padding: EdgeInsets.all(12),
+            width: _screenWidth * .35,
+            decoration: BoxDecoration(
+              color: klightDarkColor,
+              boxShadow: [
+                BoxShadow(
+                  color: const Color(0xff000000).withOpacity(0.10),
+                  blurRadius: 4.0,
+                  offset: const Offset(0.0, 3.0),
+                )
+              ],
+              borderRadius: BorderRadius.circular(50.0),
+            ),
+            child: Row(
+              children: tabs
+                  .map((tab) => TabBtn(
+                        tab: tab,
+                        click: () {
+                          setState(() {
+                            tabs.forEach((element) {
+                              element.isSelected = false;
+                            });
+                            tab.isSelected = true;
+                          });
+                        },
+                      ))
+                  .toList(),
+            ),
+          ),
+          SizedBox(height: 30),
           Consumer<Projects>(
-            builder: (context, value, __) => value.projects.isEmpty
-                ? CustomLoadingWidget()
-                : ListView.builder(
-                    itemBuilder: (context, index) =>
-                        ProjectCard(project: value.projects[index]),
-                    itemCount:
-                        value.projects.isEmpty ? 0 : value.projects.length,
-                    shrinkWrap: true,
-                  ),
+            builder: (context, value, __) {
+              List<Project> projects = [];
+              projects = value.projects
+                  .where((element) => tabs.first.isSelected
+                      ? element.isPersonal
+                      : !element.isPersonal)
+                  .toList();
+              return value.projects.isEmpty
+                  ? CustomLoadingWidget()
+                  : ListView.builder(
+                      itemBuilder: (context, index) =>
+                          ProjectCard(project: projects[index]),
+                      itemCount: projects.isEmpty ? 0 : projects.length,
+                      shrinkWrap: true,
+                    );
+            },
           ),
           const SizedBox(
             height: 20,
